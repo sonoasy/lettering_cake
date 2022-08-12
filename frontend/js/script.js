b58d2076b56030323b084a6d99d50480e6363e17
mijinkoo <starmj09@naver.com>
2022-08-12 14:31:49 +0900

    feat: 텍스트 박스 드래그 앤 드랍 기능 추가

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,4 +1,40 @@
 function printName()  {
     const name = document.getElementById('name').value;
     document.getElementById("result").innerText = name;
+}
+
+/* 텍스트 박스 드래그앤 드랍을 위한 코드 */
+const container = document.querySelector(".area");
+const box = document.querySelector(".area_box");
+
+const {width: containerWidth, height: containerHeight} = container.getBoundingClientRect();
+const {width: boxWidth, height: boxHeight} = box.getBoundingClientRect();
+
+let isDragging = null;
+let originLeft = null;
+let originTop = null;
+let originX = null;
+let originY = null;
+
+box.addEventListener("mousedown", (e) => {
+  isDragging = true;
+  originX = e.clientX;
+  originY = e.clientY;
+  originLeft = box.offsetLeft;
+  originTop = box.offsetTop;
+});
+
+document.addEventListener("mouseup", (e) => {
+  isDragging = false;
+});
+
+document.addEventListener("mousemove", (e) => {
+  if(isDragging) {
+    const diffX = e.clientX - originX;
+    const diffY = e.clientY - originY;
+    const endOfXPoint = containerWidth - boxWidth;
+    const endOfYPoint = containerHeight - boxHeight;
+    box.style.left = `${Math.min(Math.max(0, originLeft + diffX), endOfXPoint)}px`;
+    box.style.top = `${Math.min(Math.max(0, originTop + diffY), endOfYPoint)}px`;
   }
+});
